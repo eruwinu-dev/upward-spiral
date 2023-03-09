6af2071966585201c3091b894e7da0f59827d01c
Erwin Joshua Encisa <erwin.joshua.encisa@eee.upd.edu.ph>
2023-03-09 19:39:37 +0800

    Test date

--- src/pages/api/date/get.ts
+++ src/pages/api/date/get.ts
@@ -1,13 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { addDays, isToday } from "date-fns"
+import { toDateTimeString } from "@/utils/dates"
 
 type Data = {
-    isToday: Date
+    dateTime: Date
+    dateString: string
 }
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    const { week, day, date } = req.body
-
-    res.status(200).json({ isToday: date })
+    const dateTime = new Date(Date.now())
+    res.status(200).json({ dateTime, dateString: toDateTimeString(dateTime) })
 }
 
 export default handler
