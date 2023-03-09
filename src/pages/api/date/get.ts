db094c8b7d563b8e49da1e33c04fc5bb4ed9ed25
Erwin Joshua Encisa <erwin.joshua.encisa@eee.upd.edu.ph>
2023-03-09 20:21:42 +0800

    Testing UTC dates

--- src/pages/api/date/get.ts
+++ src/pages/api/date/get.ts
@@ -6,14 +6,18 @@ type Data = {
     dateTime: Date
     dateString: string
     offset: number
+    timezone: string
 }
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     const dateTime = new Date(Date.now())
+
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
     res.status(200).json({
         dateTime,
         dateString: toDateTimeString(dateTime),
         offset: dateTime.getTimezoneOffset(),
+        timezone,
     })
 }
 
